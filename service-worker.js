6dcbd466d874fd0f0c8c2ce3e7ba2bbe55d6a3e6
Pacio <pacio@Air-Pacio.home>
2026-07-11 18:52:34 +0100

    Peek panelu Tematy: 60% wzgledem ekranu (nie sztywnego 270px panelu)

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
 
-const CACHE_NAME = 'brifup-cache-v12';
+const CACHE_NAME = 'brifup-cache-v13';
 // UWAGA: index.html NIE jest tu precache'owany — patrz komentarz przy jego fetch-handlerze niżej.
 const STATIC_ASSETS = [
   './manifest.json',
